596f0da0949ca0f04a31f13de3dba624c4033260
Alex Hued <66076614+HuedCode88@users.noreply.github.com>
2024-08-09 10:07:19 -0400

    Update PlayerController.cs

--- 2D Fighting Game/Assets/PlayerController.cs
+++ 2D Fighting Game/Assets/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]public float moveSpeed = 3.0f; // Default move speed
+    [SerializeField]public float rotateSpeed = 3.0f; // Default rotate speed
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CharacterController controller = GetComponent<CharacterController>();
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float speed = moveSpeed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * speed);
+
     }
 }
